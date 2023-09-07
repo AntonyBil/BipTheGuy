3e9c2d341f7dea1164f91e7b2b6696470feefe9c
AntonyBil <beletskiy.ant@gmail.com>
2023-09-07 20:38:07 +0300

    Initial commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by apple on 07.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
