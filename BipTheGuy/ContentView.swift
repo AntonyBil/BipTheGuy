b9e4556a6b3c686c6b7fdc83c0e8fac38f50a736
AntonyBil <beletskiy.ant@gmail.com>
2023-09-07 22:39:57 +0300

    Add sounds ang Tap gesture

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,13 +6,53 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    @State private var animateImage = true
+    
     var body: some View {
         VStack {
-         
+            Spacer()
+            
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .scaleEffect(animateImage ? 1.0 : 0.8)
+                .onTapGesture {
+                    playSound(soundName: "punchSound")
+                    animateImage = false //using .scaleEffect to 90% of size
+                    withAnimation(.spring(response: 0.3, dampingFraction: 0.3)) {
+                        animateImage = true //using .scaleEffect to 100% of size
+                    }
+                }
+                
+            
+            Spacer()
+            
+            Button {
+                //TODO: Button action here
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+            .buttonStyle(.borderedProminent)
+
         }
         .padding()
+    }
+    
+    func playSound(soundName: String) {
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("Culd not reed file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("Error: \(error.localizedDescription) creating audioPlayer.")
+        }
     }
 }
 
